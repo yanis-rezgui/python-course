a3f0b44ff52f40a46912082faa11c6b2b34f0cf7
Yanis Rezgui <yanisrezgui28@gmail.com>
2026-08-08 23:21:31 +0100

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,19 @@
+def greet(name):
+
+    return f"HEllo {name}"
+
+
+def sum(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mult(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
